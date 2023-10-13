6f869e912edcfe881522edda29c8635a47c89e04
manicho <manicho@gmail.com>
2023-10-12 22:28:54 -0300

    added movie detail screen, getting all the data, only left to handle the error when cant get some data

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -1,10 +1,195 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import {
+  fetchMovieCredits,
+  fetchMovieDetails,
+  fetchSimilarMovies,
+  image500,
+} from "../../utils/moviesapi";
+import { ChevronLeftIcon, HeartIcon } from "react-native-heroicons/outline";
+import Loading from "../components/Loading";
+import Cast from "../components/Cast";
+import PopularMovies from "../components/PopularMovies/PopularMovies";
+
+const { width, height } = Dimensions.get("window");
 
 export default function MovieScreen() {
+  const { params: item } = useRoute();
+  const navigation = useNavigation();
+  const [movie, setMovie] = useState({});
+  const [cast, setCast] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getMovieDetails(item.id);
+    getMovieCredits(item.id);
+    getSimilarMovies(item.id);
+  }, [item]);
+
+  const getMovieDetails = async (id) => {
+    const data = await fetchMovieDetails(id);
+
+    setLoading(false);
+    if (data) setMovie({ ...movie, ...data });
+  };
+
+  const getMovieCredits = async (id) => {
+    const data = await fetchMovieCredits(id);
+
+    if (data) setCast(data.cast);
+  };
+
+  const getSimilarMovies = async (id) => {
+    const data = await fetchSimilarMovies(id);
+
+    if (data && data.results) setSimilarMovies(data.results);
+  };
+  console.log("similar movies: ", similarMovies);
+
+  const formatPopularity = (popularity) => {
+    const percentage = (popularity / 1000) * 170;
+
+    return `${Math.round(percentage)} %`;
+  };
+
+  const formatRuntime = (runtime) => {
+    const hours = Math.floor(runtime / 60);
+    const minutes = runtime % 60;
+
+    if (hours === 0) {
+      return `${minutes}min`;
+    } else if (minutes === 0) {
+      return `${hours}h`;
+    } else {
+      return `${hours}h ${minutes}mins`;
+    }
+  };
+
+  // console.log("Movie Details: ", movie);
+
   return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        paddingBottom: 20,
+      }}
+      className="flex-1 ng-neutral-900"
+    >
+      {/* Back Button and Movie Poster*/}
+      <View className="w-full">
+        {/* Back and Heart Icon */}
+        <View className="z-20 w-full flex-row justify-between items-center px-4 mt-12 absolute">
+          {/* Back Icon */}
+          <View className="bg-[#2496ff] p-2 rounded-full items-center justify-center">
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <ChevronLeftIcon size={30} strokeWidth={2} color="white" />
+            </TouchableOpacity>
+          </View>
+
+          {/* Heart Icon */}
+          <View className="bg-[#2496ff] p-2 rounded-full items-center justify-center">
+            <TouchableOpacity>
+              <HeartIcon size={30} strokeWidth={2} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Movie Image */}
+        {loading ? (
+          <Loading />
+        ) : (
+          <View>
+            <Image
+              source={{
+                uri: image500(movie.poster_path),
+              }}
+              style={{
+                width,
+                height: height * 0.55,
+              }}
+            />
+          </View>
+        )}
+      </View>
+
+      {/* Movie Details */}
+      <View
+        className="space-y-3 flex-1 bg-white relative py-4 mt-[-98] overflow-hidden"
+        style={{
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+        }}
+      >
+        <Image
+          source={require("../../assets/gradient2.jpg")}
+          style={{
+            width,
+            height,
+          }}
+          resizeMode="cover"
+          className="absolute top-0 left-0"
+        />
+
+        {/* Movie Title */}
+        <View className="space-y-3 p-4">
+          <Text className="text-white text-left text-2xl font-bold tracking-widest">
+            {movie?.title}
+          </Text>
+
+          {/* Genres */}
+          <Text className="flex-row space-x-2">
+            {movie?.genres?.map((genre, index) => {
+              let showDot = index + 1 != movie.genres.length;
+
+              return (
+                <Text
+                  key={index}
+                  className="text-neutral-400 font-semibold text-base text-center"
+                >
+                  {genre?.name} {showDot ? "• " : null}
+                </Text>
+              );
+            })}
+          </Text>
+
+          {/* Release Year and Runtime */}
+          {movie?.id ? (
+            <View className="bg-[#2496ff] p-2 w-3/4 rounded-lg">
+              <Text className="text-white font-semibold text-base text-left">
+                {formatPopularity(movie?.popularity)}
+                {" * "}
+                {formatRuntime(movie?.runtime)} {}{" "}
+                {movie?.release_date?.split("-")[0] || "N/A"}
+              </Text>
+            </View>
+          ) : null}
+
+          {/* Description */}
+          <Text className="text-neutral-300 text-sm tracking-widest leading-6">
+            {movie?.overview}
+          </Text>
+
+          {/* Cast */}
+          {movie?.id && cast.length > 0 && (
+            <Cast navigation={navigation} cast={cast} />
+          )}
+
+          {/* Similar Movies */}
+          {movie?.id && similarMovies.length > 0 && (
+            <PopularMovies title="Similar Movies" data={similarMovies} />
+          )}
+        </View>
+      </View>
+    </ScrollView>
   );
 }
--- utils/moviesapi.js
+++ utils/moviesapi.js
@@ -8,6 +8,16 @@ const upComingMoviesEndpoint = `${apiBaseUrl}/movie/upcoming?api_key=${movieapik
 const topRatedMoviesEndpoint = `${apiBaseUrl}/movie/top_rated?api_key=${movieapikey}`;
 const genresEndpoint = `${apiBaseUrl}/genre/movie/list?api_key=${movieapikey}`;
 
+// Movie Details Endpoint
+const movieDetailsEndpoint = (id) =>
+  `${apiBaseUrl}/movie/${id}?api_key=${movieapikey}`;
+
+const movieCreditsEndpoint = (id) =>
+  `${apiBaseUrl}/movie/${id}/credits?api_key=${movieapikey}`;
+
+const similarMoviesEndpoint = (id) =>
+  `${apiBaseUrl}/movie/${id}/similar?api_key=${movieapikey}`;
+
 const movieApiCall = async (endpoints, params) => {
   const options = {
     method: "GET",
@@ -16,8 +26,8 @@ const movieApiCall = async (endpoints, params) => {
   };
 
   try {
-    const { data } = await axios.request(options);
-    return data;
+    const response = await axios.request(options);
+    return response.data;
   } catch (error) {
     console.log(error);
     return {};
@@ -46,4 +56,16 @@ export const fetchTopRatedMovie = () => {
 
 export const fetchGenres = () => {
   return movieApiCall(genresEndpoint);
+};
+
+export const fetchMovieDetails = (id) => {
+  return movieApiCall(movieDetailsEndpoint(id));
+};
+
+export const fetchMovieCredits = (movieId) => {
+  return movieApiCall(movieCreditsEndpoint(movieId));
+};
+
+export const fetchSimilarMovies = (movieId) => {
+  return movieApiCall(similarMoviesEndpoint(movieId));
 };
